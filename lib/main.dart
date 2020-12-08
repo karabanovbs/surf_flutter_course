7aedd285077477f81baf634025bb36852b87eeb9
Boris Karabanov <karabanovbswork@gmail.com>
2020-12-08 15:24:05 +0500

    [4-1] create widget with scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(App());
@@ -13,7 +14,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyFirstWidget(),
+      home: SightListScreen(),
     );
   }
 }
