d84ffdc2b727f51024c057591ae02affac9ad036
Boris Karabanov <karabanovbswork@gmail.com>
2020-12-10 18:47:46 +0500

    [4-2-1] text widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/theme/colors.dart';
 import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
@@ -12,97 +13,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'App title',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: AppColorsWhite.white,
+        backgroundColor: AppColorsWhite.white,
       ),
       home: SightListScreen(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class MyFirstWidget extends StatelessWidget {
-  int _counter = 0;
-
-  // Type getContextType() {
-  //   return context.runtimeType;
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    print(_counter++);
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
-    );
-  }
-}
-
-class MySecondWidget extends StatefulWidget {
-  @override
-  _MySecondWidgetState createState() => _MySecondWidgetState();
-}
-
-class _MySecondWidgetState extends State<MySecondWidget> {
-  int _counter = 0;
-
-  Type getContextType() {
-    return context.runtimeType;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print(_counter++);
-    return Container(
-      child: Center(
-        child: Text('Hello!'),
-      ),
     );
   }
 }
--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:places/theme/colors.dart';
 
+/// App home screen with list of sight
 class SightListScreen extends StatefulWidget {
   @override
   _SightListScreenState createState() => _SightListScreenState();
@@ -8,6 +10,31 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      /// Create AppBar
+      appBar: AppBar(
+        /// Set color by backgroundColor
+        backgroundColor: Theme.of(context).backgroundColor,
+
+        /// Remove shadow
+        elevation: 0,
+        /// Set AppBar height
+        toolbarHeight: 72,
+
+        /// Set title
+        title: Text(
+          'Список\nинтересных мест',
+          /// Set text align
+          textAlign: TextAlign.left,
+          maxLines: 2,
+          style: TextStyle(
+            color: AppColorsWhite.secondary,
+            fontSize: 32,
+            height: 36 / 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
