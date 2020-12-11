0a3a60e3cb7fec6374f340734d88d89539174af5
Boris Karabanov <karabanovbswork@gmail.com>
2020-12-11 20:23:03 +0500

    [fix] by review

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:places/theme/colors.dart';
 
+/// Constants
+const double appBarPaddingTop = 40;
+const double appBarPaddingBottom = 16;
+const double appBarPaddingLeft = 16;
+const double appBarPaddingRight = 16;
+const double appBarTitleHeight = 72;
+const double totalAppBarHeight =
+    appBarPaddingTop + appBarTitleHeight + appBarPaddingBottom;
+
 /// App home screen with list of sight
 class SightListScreen extends StatefulWidget {
   @override
@@ -20,24 +29,33 @@ class _SightListScreenState extends State<SightListScreen> {
         elevation: 0,
 
         /// Set AppBar height
-        toolbarHeight: 72,
-        title: RichText(
-          maxLines: 2,
-          text: TextSpan(
-            style: TextStyle(
-              color: AppColorsWhite.secondary,
-              fontSize: 32,
-              height: 36 / 32,
-              fontWeight: FontWeight.bold,
+        toolbarHeight: totalAppBarHeight,
+        title: Padding(
+          padding: const EdgeInsets.fromLTRB(appBarPaddingLeft,
+              appBarPaddingTop, appBarPaddingRight, appBarPaddingBottom),
+          child: SizedBox(
+            height: appBarTitleHeight,
+            width: double.infinity,
+            child: RichText(
+              maxLines: 2,
+              text: TextSpan(
+                style: TextStyle(
+                  color: AppColorsWhite.secondary,
+                  fontSize: 32,
+                  height: 36 / 32,
+                  fontWeight: FontWeight.bold,
+                ),
+                children: [
+                  TextSpan(
+                      text: 'С', style: TextStyle(color: AppColorsWhite.green)),
+                  TextSpan(text: 'писок\n'),
+                  TextSpan(
+                      text: 'и',
+                      style: TextStyle(color: AppColorsWhite.yellow)),
+                  TextSpan(text: 'нтересных мест'),
+                ],
+              ),
             ),
-            children: [
-              TextSpan(
-                  text: 'С', style: TextStyle(color: AppColorsWhite.green)),
-              TextSpan(text: 'писок\n'),
-              TextSpan(
-                  text: 'и', style: TextStyle(color: AppColorsWhite.yellow)),
-              TextSpan(text: 'нтересных мест'),
-            ],
           ),
         ),
       ),
