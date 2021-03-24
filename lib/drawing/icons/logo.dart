c32b3bad76a7b954244407cd2daa3dd1ee7947c3
Boris Karabanov <karabanovbswork@gmail.com>
2021-03-24 15:33:31 +0500

    [7-2] futures

---
+++ lib/drawing/icons/logo.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:svg_to_path_annotation/svg_to_path_annotation.dart';
+
+part 'logo.g.dart';
+
+@SvgSource('res/svg/logo.svg')
+class LogoIcon extends _LogoIcon {
+  const LogoIcon();
+}
