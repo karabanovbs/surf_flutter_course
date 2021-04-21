faeab3be4f4280d620f644c1cae2e780ab72a5b8
Boris Karabanov <karabanovbswork@gmail.com>
2021-04-21 14:06:53 +0500

    [12-2-1] exceptions

---
+++ lib/exceptions/exceptions.dart
@@ -0,0 +1,1 @@
+export 'networking.dart';
---
+++ lib/exceptions/networking.dart
@@ -0,0 +1,14 @@
+class NetworkException implements Exception {
+  final String requestUrl;
+  final String error;
+
+  NetworkException(
+    this.requestUrl,
+    this.error,
+  );
+
+  @override
+  String toString() {
+    return "В запросе '${requestUrl}' возникла ошибка: ${error}";
+  }
+}
